cce8c8efba2ea83b4207795d0ec03757e55a1f15
Jack Rosenthal <jack@rosenth.al>
2015-01-16 12:43:54 -0700

    Add console keymap support, fix problems if they have keyboardctl

--- linux/install.sh
+++ linux/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cp $DIR/xkb/symbols/wuly /usr/share/X11/xkb/symbols/
-cp $DIR/xkb/10-keyboard.conf /etc/X11/xorg.conf.d/
+cp $DIR/xkb/99-keyboard.conf /etc/X11/xorg.conf.d/
+mkdir -p /usr/share/kbd/keymaps/i386/wuly/
+cp $DIR/ckb/wuly.map.gz /usr/share/kbd/keymaps/i386/wuly/
+# Auto install console and x11 keymap if they have systemd
+localectl set-keymap wuly
 setxkbmap wuly
