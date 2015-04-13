25ad5e088a421fdd31e90f4fac5c8867f553e2f7
Jack Rosenthal <jack@rosenth.al>
2015-04-13 12:08:52 -0600

    Move Q

--- linux/install.sh
+++ linux/install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cp $DIR/xkb/symbols/wuly /usr/share/X11/xkb/symbols/
-cp $DIR/xkb/99-keyboard.conf /etc/X11/xorg.conf.d/
+cp $DIR/xkb/symbols/wuly2 /usr/share/X11/xkb/symbols/
+#cp $DIR/xkb/99-keyboard.conf /etc/X11/xorg.conf.d/
 mkdir -p /usr/share/kbd/keymaps/i386/wuly/
-cp $DIR/ckb/wuly.map.gz /usr/share/kbd/keymaps/i386/wuly/
-# Auto install console and x11 keymap if they have systemd
-localectl set-keymap wuly
-setxkbmap wuly
+command -v ckbcomp >/dev/null && (ckbcomp wuly | gzip -f - > /usr/share/kbd/keymaps/i386/wuly/wuly.map.gz) || $DIR/ckb/wuly.map.gz /usr/share/kbd/keymaps/i386/wuly/
+echo "wuly was installed."
+echo "If you are using systemd, use \`localectl set-keymap wuly\` to install."
+echo "To start using wuly now, run \`setxkbmap wuly\`."
+echo "...or in a console, run \`loadkeys wuly\` as root."
